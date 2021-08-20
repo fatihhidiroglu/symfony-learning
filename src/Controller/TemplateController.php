4d96c81c91dbbeac9be8721ebaf1c7709f491664
Fatih Hıdıroğlu <fatih.hidiroglu58@gmail.com>
2021-08-20 15:02:08 +0300

    template(twig) yapısı için klasör ve controller oluşturuldu

---
+++ src/Controller/TemplateController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TemplateController extends AbstractController {
+    /**
+     * @Route('/template', name='index')
+     * @return Response
+     */
+
+    public function index() {
+        return $this->render('template/index.html.twig');
+    }
+}
